6d7e941131afff6e589851646815fa206be338b6
unknown <yakubema@gmail.com>
2021-09-19 17:34:22 +0300

    add branch

--- src/main/java/ua/prog/kiev/App.java
+++ src/main/java/ua/prog/kiev/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World of Tanks!" );
+        System.out.println( "Hello Amigo!" );
             }
 }
